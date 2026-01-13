6e1f652239fef3d50380974e8e5ca3571c537d96
annabelle <annabelle@gmail.com>
2026-01-13 14:58:38 +1100

    troubleshooting

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import "7.css/dist/7.scoped.css";
-import "/src/index.css";
+import "./index.css";
 import { NoteNavProvider } from "./providers/NoteNavProvider";
 import { BrowserRouter as Router } from 'react-router-dom';
 
