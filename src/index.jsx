0716ade1c70daf541789f4c4363ad3277ce25a0c
annabelle <annabelle@gmail.com>
2026-02-14 09:39:07 +1100

    from browser routing to has routing so can share independent links

--- src/index.jsx
+++ src/index.jsx
@@ -5,14 +5,14 @@ import App from './App.jsx';
 import "7.css/dist/7.scoped.css";
 import "./index.css";
 import { NoteNavProvider } from "./providers/NoteNavProvider";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <HashRouter>
       <NoteNavProvider>
         <App />
       </NoteNavProvider>
-    </Router>
+    </HashRouter>
   </StrictMode>,
 )
