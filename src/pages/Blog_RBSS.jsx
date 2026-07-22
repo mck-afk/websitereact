8e6373f15de0f10b71d538d4d049e0a23e14e8a4
annabelle <annabelle@gmail.com>
2026-07-22 15:08:17 +1000

    fixed spelling and added icons

--- src/pages/Blog_RBSS.jsx
+++ src/pages/Blog_RBSS.jsx
@@ -17,7 +17,7 @@ const RBSS = () => {
                 <PostItNote 
                 id="noteLaraCroftGuiness"
                 className="note yellow"
-                title="'Guinness World Record recognises [Lara Croft] as the most successful human video game heroine.'"
+                title="Guinness World Record recognises [Lara Croft] as the most successful human video game heroine."
                 lastUpdated="">
                     
                     <cite className="actions"><a href="https://web.archive.org/web/20070929135538/http://uk.gamespot.com/news/6147291.html">Guy Cocker, GameSpot UK, Posted Apr 7, 2006</a></cite>
@@ -25,59 +25,61 @@ const RBSS = () => {
                 <img src="https://pbs.twimg.com/media/Eq14XSJVoAAYnAP.jpg" style={{ width: "500px" }} />
                     <p><a href="https://archive.org/details/guinnessworldrec00guin_0/page/58/mode/2up">Guinness World Record 2008 page available through the Internet Archive</a></p>
 
-                <p>I am grieving the bimboification of <a href="https://en.wikipedia.org/wiki/Lara_Croft">Lara Croft</a>, the female protagonist from the series 'Tomb Raider'. In this essay I present the case that we should care about the dramatic change in body shape from 'Tomb Raider: Legacy of Atlantis' (2027 - unreleased at time of writing) to the <a href="https://en.wikipedia.org/wiki/Tomb_Raider#Survivor_trilogy">'Survivor Trilogy'</a>: Tomb Raider (2013), Rise of the Tomb Raider (2015), and Shadow of the Tomb Raider (2018)</p> 
+                <p>I am grieving the bimboification of <a href="https://en.wikipedia.org/wiki/Lara_Croft">Lara Croft</a>, the protagonist of the series 'Tomb Raider', whose body has been shaped across studios and time periods. In this essay I argue that the dramatic change in Lara's body shape, as announced in the latest installment of the series, is a regression and degradation of her character. I will be comparing the latest installment Tomb Raider: Legacy of Atlantis–LOA (2027 - unreleased at time of writing) by studio Embracer to the <a href="https://en.wikipedia.org/wiki/Tomb_Raider#Survivor_trilogy">'Survivor Trilogy'</a>: Tomb Raider–TR (2013), Rise of the Tomb Raider–ROTR (2015), and Shadow of the Tomb Raider–SOTTR (2018) by studio Square Enix.</p> 
                 
                 <h2>Bodies in media are the opposite of neutral</h2>
                 <p>They have been shaped by multiple people, who coalesce around an ideal: in the literal size and shape of body (i.e. thin, skinny, bones protruding), but also through the more subtle manipulations of the lens through which we are viewing that body (angling, lighting, lens, posing, positioning). I embrace my sensitivity to this, because it is my body that is the intended target of these bodily shapings.</p>
 
-                <p>Outside of the canon material, there is the fandom who takes the characters/world/ideas/material and spins them into headcanons and fanfictions. As a teenage girl, this was one of the only ways my creativity was validated — that I felt I had control over something, where I could control the narrative. I could imagine my romantic relationships and explore the dynamics that come with them. I could imagine my characters in a different light. It was a reclamation: here is the material, already strong, powerful, full of personality and vitality. I wanted to be just like them.</p>
+                <p>When I met Lara in TR 2013, I was in my teenage years. Everyone who has been a teenager knows how suddenly your body becomes a site of violence–of opinion, speculation, restriction. In Lara's body, I saw freedom. Yes she was young and thin in TR, because it was presented as her origin story–she was, by definition of this context, unprepared for the adventure. What I love so much about this trilogy (alongside the fact that I found it as a pivotal time in my life) is that we can literally see her body grow stronger through the trilogy–becoming the body of an adventurer–lean, athletic, capable, strong, unstoppable. When I saw the trailer for SOTTR I involuntarily gasped upon seeing the screenshot I have included below. To see a woman–and not just any woman, but the worlds "most successful human video game heroine" who has been sexualised relentlessly since her inception–with toned, defined muscles–it was a statement. This is what the body of a woman can look like–the body of a woman who cares more about what she does than how she looks. It was a revelation for me, and I have to credit it with the health and strength I have today because I stayed exercising throughout my teenage years.</p>
+
+                
+
+
                 <PostItNote 
                 id="noteLaraCroftG"
                 className="note yellow"
-                title="'The sexualization of women is only appealing if it’s nonconsensual.'"
+                title="The sexualization of women is only appealing if it’s nonconsensual."
                 lastUpdated="">
                     
-                    <cite className="actions"><a href="https://web.archive.org/web/20070929135538/http://uk.gamespot.com/news/6147291.html">Female 'Purity' Is Bullshit<br /> by Lindy West, May 7, 2013 </a></cite>
+                    <cite className="actions"><a href="https://www.jezebel.com/female-purity-is-bullshit-493278191">Female 'Purity' Is Bullshit<br /> by Lindy West, May 7, 2013 </a></cite>
                 </PostItNote>
 
-                <p>I remember seeing the muscles on Lara Croft in the promotional material for SOTTR and feeling actively jealous and inspired to get strong in a way I have not felt inspired by Instagram models and Chloe Ting workouts.</p>
-                <p>And if it was a fad — a phase — then it was a good one in my opinion because it was girls like me, who were finding themselves, who were craving representations that were not sexualised, infantilised, or abused in the primary material (although that could be debated slightly with Lara Croft especially in the 2013 game; she does get humiliated and dirtied a lot).</p>
-                <p>I remember the awe of SOTTR, playing through the game as we move Lara around and the cutscenes get deep, cutting, self-critical reflection as she reconciles and reflects on her actions — that indeed her actions have caused great harm; even if it was framed as 'the lesser evil' as Geralt in The Last Wish says "If I’m to choose between one evil and another… I’d rather not choose at all." (which, instead of abstaining or turning a blind eye as might be the initial interpretation, I think what Andrez Sap was trying to insinuate is that Geralt chooses both; and in doing so, effectively neutralises the dimension of 'choice').</p>
-                <p>As I grow older, I watch these young women change shape as they are moulded and shown to the world through new hands. Images matter. Let's go on a tour.</p>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR6qO699clLR7VL_otNjnMsletYS7Fq27djbBYs8FWpr9BDmTEVpBX6Lg&s=10" style={{ width: "400px" }} />
+
+                <p>As I grow older, I watch these young women change shape as they are moulded and shown to the world through new hands. Images matter. And my voice, calling out the ways these images have changed, matter. So let's go on a tour.</p>
 
                 <h2>Exhibit 1: Promo Material - Action Shot</h2>
                 <p>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAVFGlWH8RyyuDHU0hU4zBxI6prWZWynuYt76rgW3EuNKntLMnJwrb5d4&s=10" alt="TR bow and arrow" style={{ width: "500px" }} />
-                    <img src="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/11/10/1447142804742/384f97dc-993b-457f-b3aa-f741186671de-620x372.jpeg?width=465&dpr=1&s=none&crop=none" alt="ROTTR bow and arrow" style={{ width: "500px" }} />
-                    <img src="https://i.ytimg.com/vi/7jX55vIAks0/hqdefault.jpg" alt="SOTTR bow and arrow" style={{ width: "500px" }} />
-                    <img src="https://cms-assets.xboxservices.com/assets/3c/ea/3cea956b-485c-4c08-9b12-206e4f726610.jpg?n=022079775_Gallery_1350x759_01.jpg" alt="LOATR bow and arrow" style={{ width: "500px" }} />
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAVFGlWH8RyyuDHU0hU4zBxI6prWZWynuYt76rgW3EuNKntLMnJwrb5d4&s=10" alt="TR bow and arrow" style={{ width: "400px" }} />
+                    <img src="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/11/10/1447142804742/384f97dc-993b-457f-b3aa-f741186671de-620x372.jpeg?width=465&dpr=1&s=none&crop=none" alt="ROTTR bow and arrow" style={{ width: "400px" }} />
+                    <img src="https://i.ytimg.com/vi/7jX55vIAks0/hqdefault.jpg" alt="SOTTR bow and arrow" style={{ width: "400px" }} />
+                    <img src="https://cms-assets.xboxservices.com/assets/3c/ea/3cea956b-485c-4c08-9b12-206e4f726610.jpg?n=022079775_Gallery_1350x759_01.jpg" alt="LOATR bow and arrow" style={{ width: "400px" }} />
                 </p> 
-                <p>The promotional material from the trilogy shows her engaged in action scenes, highlighting her strength and agility. She is looking at her target, alert and aware, her head tilted either up in alertness, or tilted down in concerntation. The promotional material for LOA, one of her hands is engaged in something she can't see. It's an inherently vulnerable position: arms akimbo like legs spread wide, breasts on full display, and don't even get me started on the sliver of exposed midriff. She's t-posing; a meme - humiliation.</p>
+                <p>The promotional material from the trilogy shows her engaged in action, highlighting her strength and agility. She is looking at her target, alert and aware, her head slightly tilted either up in alertness, or down in concentration. The promotional material for LOA, one of her hands is engaged in something she can't see. It's an inefficent and dangerous pose for an adventurer to be in, and, in the broader context of our society, arms spread wide like this makes me think of the <a href="https://en.wikipedia.org/wiki/T-pose">T-Pose meme</a>. Don't even get me started on the sliver of exposed midriff–we are here to talk about her body, not her clothes.</p>
 
                 <h2>Exhibit 2: Body - Bicep screenshot from trailer</h2>
                 <p>
-                    <a href="https://www.youtube.com/watch?v=XsWojNNqFTg"><img src="images/tr_bicep.png" alt="TR bicep" style={{ width: "500px" }} /></a>
-                    <a href="https://www.youtube.com/watch?v=qiYiddjc6cU"><img src="images/rotr_bicep.png" alt="ROTTR bicep" style={{ width: "500px" }} /></a>
-                    <a href="https://www.youtube.com/watch?v=XYtyeqVQnRI"><img src="images/sottr_bicep.png" alt="SOTTR bicep" style={{ width: "500px" }} /></a>
-                    <a href="https://www.youtube.com/watch?v=n6JxVxSHjqE"><img src="images/loa_bicep.png" alt="LOATR bicep" style={{ width: "500px" }} /></a>
+                    <a href="https://www.youtube.com/watch?v=XsWojNNqFTg"><img src="images/tr_bicep.png" alt="TR bicep" style={{ width: "400px" }} /></a>
+                    <a href="https://www.youtube.com/watch?v=qiYiddjc6cU"><img src="images/rotr_bicep.png" alt="ROTTR bicep" style={{ width: "400px" }} /></a>
+                    <a href="https://www.youtube.com/watch?v=XYtyeqVQnRI"><img src="images/sottr_bicep.png" alt="SOTTR bicep" style={{ width: "400px" }} /></a>
+                    <a href="https://www.youtube.com/watch?v=n6JxVxSHjqE"><img src="images/loa_bicep.png" alt="LOATR bicep" style={{ width: "400px" }} /></a>
                 </p> 
                 <p>The way women's bodies are presented tells us about the societal attitudes of the time. Surprise: authoritarian regimes are back in power and controlling women's bodies by idealising smaller, thinner, smoother bodies. It is the first step in every regime. They obliterated the triceps, shaved her biceps to the barest of bumps, and smoothed over the shoulder. Lara Croft's body has <a href="https://www.infinitefrontiers.org.uk/no-lara-crofts-body-didnt-get-a-150-enhancement-and-it-shouldnt-matter/">always been a statement piece, whether you believe the rumours or not</a>, especially considering the creator Toby Gard soon left the studio because <a href="https://tombraider.fandom.com/wiki/Toby_Gard">"Lara was becoming too much of a sex symbol rather than the independent strong female heroine he had intended her to be".</a></p>
                 <p>Also, in LOA, why did they design her to have the face of a mature woman but the body of an 18 year old? At least in the earlier games, we knew she was a young woman and her body and face matched.</p>
-                <p><b>July 13 2026 addition -</b> Upon further consideration, I understand where this chimera comes from. It's the moulding of <a href="https://culturevulture.substack.com/p/on-keeping-women-skinny-so-they-dont">authoritarian</a> and <a href="https://finalgirldigital.substack.com/p/pretty-babies-exploring-epstein-and">pedophiliac</a> beauty ideals (thin, toned, smooth, flawless).
-                <br /> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR6qO699clLR7VL_otNjnMsletYS7Fq27djbBYs8FWpr9BDmTEVpBX6Lg&s=10" style={{ width: "400px" }} /></p>
+                <p><b>July 13 2026 addition -</b> Upon further consideration, I understand where this chimera comes from. It's the moulding of <a href="https://culturevulture.substack.com/p/on-keeping-women-skinny-so-they-dont">authoritarian</a> and <a href="https://finalgirldigital.substack.com/p/pretty-babies-exploring-epstein-and">pedophiliac</a> beauty ideals (thin, toned, smooth, flawless).</p>
 
                 <h2>Exhibit 3: Promo Material - Title Screen</h2>
                 <p>
-                    <img src="https://i.ytimg.com/vi/SODIOOEdqro/maxresdefault.jpg" alt="TR title" style={{ width: "500px" }} />
-                    <img src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/391220/header.jpg?t=1766187095" alt="ROTTR title" style={{ width: "500px" }}/>
-                    <img src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/750920/header.jpg?t=1765239925" alt="SOTTR title" style={{ width: "500px" }}/>
-                    <img src="https://xboxwire.thesourcemediaassets.com/sites/2/2025/12/TR_ATL_KEYART_EARNED_PLACEMENTS_169_3840x2160_CT-000023923-d8ecfde324a2de01650d.jpg" alt="LOATR title" style={{ width: "500px" }} />
+                    <img src="https://i.ytimg.com/vi/SODIOOEdqro/maxresdefault.jpg" alt="TR title" style={{ width: "400px" }} />
+                    <img src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/391220/header.jpg?t=1766187095" alt="ROTTR title" style={{ width: "400px" }}/>
+                    <img src="https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/750920/header.jpg?t=1765239925" alt="SOTTR title" style={{ width: "400px" }}/>
+                    <img src="https://xboxwire.thesourcemediaassets.com/sites/2/2025/12/TR_ATL_KEYART_EARNED_PLACEMENTS_169_3840x2160_CT-000023923-d8ecfde324a2de01650d.jpg" alt="LOATR title" style={{ width: "400px" }} />
                 </p> 
-                <p>From the dirt and grit of survival, indicating the toll it takes on the mind, body, and soul - to the shiny toned thighs and manicured hair.</p>
+                <p>All the dirt and grit of survival, the environmental storytelling of the toll that adventuring takes on her mind, body, and soul, has been scrubbed clean, into shiny thighs and manicured hair. Looking at the positioning of the body, in all of the trilogy title screens, Lara's body is staged in a position that makes sense int he environment of the game. In TR, both feet planted and standing tall, this is the story of her first adventure. Her center of gravity is over one leg, which is not a very stable position for an adventurer who needs to be able to react and move quickly. In ROTR, her center of gravity is dropped into an active stance, she is ready to spring into action. In SOTTR she is perched, hinting to the newly added game functionality; again it's like we've caught her in a moment of action. In LOA, in what circumstance would her leg need to be up on that rock? In what world is that a more stable center of gravity? To be frank, it actually reminds of me of her body positioning in TR, given that it's the single leg standing–indicating an unbalancedness. She's become a doll – all right angled joints.</p>
                 <h2>I know it's a reboot. I know it's a different studio. I know it's targeting a different audience. I am still enraged.</h2>
-                <p>Lara Croft is not just any female protagonist. What does it mean when we humanise a "sex symbol" and give her personality, guilt, shame, fear, arrogance, joy, revelation, healing, peace — only to take it away again? I see the <a href="https://www.britannica.com/event/Roe-v-Wade">pattern of rolling back women's rights</a> — I can't shake the feeling that this, being so blatant, is another assault.</p>
-                <p>Of course Lara became a sex symbol precisely because she was intended to not be. Isn't this all painfully obvious — like bringing hot tea to your lips that was too hot to sip a mere moment ago. </p>
-                <p>Bodies - epseically the bodies that are presented in media - impact us. They communicate what kinds of bodies are safe where. The bimboification of Lara Croft doesn't make my body feel safe. How does it make yours?</p>
+                <p>Lara Croft is not just any female protagonist. What does it mean when we humanise a "sex symbol" and give her personality, guilt, shame, fear, arrogance, joy, revelation, healing, peace–only to take it away again? I see the <a href="https://www.britannica.com/event/Roe-v-Wade">pattern of rolling back women's rights</a>, I can't shake the feeling that this, being so blatant, is another assault.</p>
+                <p>Of course Lara became a sex symbol precisely because she was intended to not be. Isn't this all painfully obvious—like bringing hot tea to your lips that was too hot to sip a mere moment ago. </p>
+                <p>Bodies-especially the bodies that are presented in media-impact us. They communicate what kinds of bodies are safe where. The bimboification of Lara Croft doesn't make my body feel safe. How does it make yours?</p>
 
             <div className="red" style={{ paddingBottom: "5px" }}>
                 <h1>thank you for reading</h1>
